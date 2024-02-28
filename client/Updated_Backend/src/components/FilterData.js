da9216a36ed206c035be0c5674e68eddfddcff8c
Himanshu-Sangshetti <himanshusangshetty@gmail.com>
2024-02-28 12:33:25 +0530

    Changes

---
+++ client/Updated_Backend/src/components/FilterData.js
@@ -0,0 +1,16 @@
+export const filterData = [
+  {
+    id: "1",
+    title: "Assignments",
+  },
+  {
+    id: "2",
+    title: "Theory Topics",
+  },
+  {
+    id: "3",
+    title: "Interview questions",
+  },
+  
+];
+export const apiUrl = "https://54.224.207.176:8080/api/assignment/getAllAssignments";
--- server/Os_VirtualLab_Backend-main/README.md
+++
@@ -1,5 +0,0 @@
-# Os_VirtualLab_Backend
-Technologies Used 
-Node.js 
-MongoDB
-Express
